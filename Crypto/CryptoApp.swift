7df27997a4d7e35434aaf9718fa3fe2c34a591a3
Bartosz Pawełczyk <bartosz.pawelczyk@miquido.com>
2022-11-09 20:04:30 +0100

    feat(init): initial commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Bartosz Pawełczyk on 09/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
